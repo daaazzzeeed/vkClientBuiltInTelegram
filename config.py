bc8b18bf2bb15e93e8c3619d74d353af149247a4
Petukhov Dmitry <petumit@yandex.ru>
2018-03-02 00:33:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 ApiToken = '488195156:AAEsw8Yt19tRL2Bnnys_fVV9Y_Zq5TAwfPI'
-vkLogin = 'dmipetu@yandex.ru'
-vkPassw = 'Fsociety2019'
+vkLogin = 'dm@yandex.ru'
+vkPassw = 'F-------2019'
 appId = '5580473'
 myId = '32432594'
 friends_dict = dict()
